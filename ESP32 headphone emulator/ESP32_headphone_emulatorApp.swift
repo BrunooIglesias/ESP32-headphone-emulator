67aae5803207c0401d42277b3805ee816218d25d
Bruno Silva <111820238+BrunooIglesias@users.noreply.github.com>
2025-04-11 11:49:08 -0300

    Initial Commit

---
+++ ESP32 headphone emulator/ESP32_headphone_emulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ESP32_headphone_emulatorApp.swift
+//  ESP32 headphone emulator
+//
+//  Created by Bruno Sebastian Silva Iglesias on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ESP32_headphone_emulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
